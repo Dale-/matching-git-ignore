8904cb7d8b122b2f68970e5f85cc48478ef603a1
Dale <18717394542@163.com>
2015-06-15 20:44:50 +0800

    add function of generateRule in matching_ignore

--- src/main/scala/com/thoughtworks/pli/remotepair/core/matching_ignore.scala
+++ src/main/scala/com/thoughtworks/pli/remotepair/core/matching_ignore.scala
@@ -2,16 +2,18 @@ package com.thoughtworks.pli.remotepair.core
 
 class matching_ignore {
 
-  def generateRule(ignoreFile: String): List[String] = {
+  def generateRule(ignoreFile: String): Array[String] = {
     val ignoreArray = ignoreFile.split("\n")
     val ignoreInfo = diminishBlankLine(ignoreArray)
     val ignoreRule = new Array[String](ignoreInfo.length)
     for (i <- 0 until ignoreInfo.length) {
       if(ignoreInfo(i).contains("*.")) {
         ignoreRule(i) = ignoreInfo(i).substring(1, ignoreInfo(i).length)
+      } else {
+        ignoreRule(i) = ignoreInfo(i)
       }
     }
-
+    return ignoreRule
   }
 
   def diminishBlankLine(array: Array[String]): Array[String] = {
