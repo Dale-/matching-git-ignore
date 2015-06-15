b97c6dee64325bf1252debdbff563004512c7c79
Dale <18717394542@163.com>
2015-06-15 17:05:59 +0800

    create project structure

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "matching-git-ignore"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala/com/thoughtworks/pli/remotepair/core/matching_ignore.scala
@@ -0,0 +1,8 @@
+package com.thoughtworks.pli.remotepair.core
+
+/**
+ * Created by Dale on 6/15/15.
+ */
+class matching_ignore {
+
+}
