8d512486c3c6a79ebb4cbf6a814643d9068a3beb
Dale <18717394542@163.com>
2015-06-15 20:05:57 +0800

    modify the type of function param from list to array

--- src/main/scala/com/thoughtworks/pli/remotepair/core/matching_ignore.scala
+++ src/main/scala/com/thoughtworks/pli/remotepair/core/matching_ignore.scala
@@ -2,13 +2,20 @@ package com.thoughtworks.pli.remotepair.core
 
 class matching_ignore {
 
-//  def generateRule(ignoreFile: String): List[String] = {
-//    val ignoreList = ignoreFile.split("\n").toList
-//
-//  }
+  def generateRule(ignoreFile: String): List[String] = {
+    val ignoreArray = ignoreFile.split("\n")
+    val ignoreInfo = diminishBlankLine(ignoreArray)
+    val ignoreRule = new Array[String](ignoreInfo.length)
+    for (i <- 0 until ignoreInfo.length) {
+      if(ignoreInfo(i).contains("*.")) {
+        ignoreRule(i) = ignoreInfo(i).substring(1, ignoreInfo(i).length)
+      }
+    }
 
-  def diminishBlankLine(list: List[String]): List[String] = {
-    list.filter(!_.isEmpty)
+  }
+
+  def diminishBlankLine(array: Array[String]): Array[String] = {
+    array.filter(!_.isEmpty)
   }
 
 }
